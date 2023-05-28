a93fc5533a4c043e920f131db308063282cbd132
jheymis rodrigues <rodriguesjheymis@gmail.com>
2023-05-28 02:11:11 -0300

    :lipstick:

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,7 +1,20 @@
 export const Theme = {
   background:
     "linear-gradient(111.51deg, #BB1EE8 -0.26%, rgba(138, 31, 242, 0.8) 100%)",
-  "purple-light": "#BB1EE8",
-  "bluish-purple": "#8A1FF2",
-  "bluish-purple-80": "#8a1ff2cc",
+  "primary-purple-light": "#BB1EE8",
+  "primary-bluish-purple": "#8A1FF2",
+  "primary-bluish-purple-80": "#8a1ff2cc",
+  "primary-white": "#FFFFFF",
+  "primary-purple-content-field": "#ac044a0d",
+  "primary-header-30": "#7A0DCF",
+  "secondary-black-button": "#111010",
+  "secondary-create-task-input": "#111010",
+  "secondary-task-to-do": "#FEAD0F",
+  "secondary-secondary-task-done": "#FFFFFF",
+  "secondary-tools": "#021004",
+  "secondary-tools-alert": "#E80916",
+  "secondary-tools-disabled": "9B9B9B",
+  "secondary-task-date-time": "#021004",
+  "secondary-logout": "#D4D1DC",
+
 };
